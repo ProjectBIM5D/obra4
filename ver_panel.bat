73bf408aae2248e7b00402b93f0ed903162a42ff
ProjectManager <albertalayo5@gmail.com>
2026-09-22 15:01:23 -0500

    bien

---
+++ ver_panel.bat
@@ -0,0 +1,4 @@
+﻿@echo off
+rem UN SOLO servidor local para todo: visor 3D (raiz) + panel/dashboard/parte diario (dashboard\).
+start "" http://localhost:8300/dashboard/panel.html
+python -m http.server 8300
